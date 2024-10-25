033ca256a89761a0856f051045bd110b015ee5b8
Sergei Rodin <sm6078@mail.ru>
2024-10-25 03:05:36 +0300

    Update homework, add profiles

--- shop-drugs/src/main/java/com/javaacademy/RunnerTest.java
+++ shop-drugs/src/main/java/com/javaacademy/RunnerTest.java
@@ -4,7 +4,7 @@ import static java.math.BigDecimal.valueOf;
 
 public class RunnerTest {
     public static void main(String[] args) {
-        Drug lorotadin = new Drug("Лоротадин", valueOf(250), false);
+        Drug lorotadin = new Drug("Лоротадин", valueOf(250), true);
         CashRegister cashRegister = new CashRegister();
         cashRegister.addSoldProduct(lorotadin);
         cashRegister.printSoldProducts();
